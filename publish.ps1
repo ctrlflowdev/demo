d35aee9e6d086c6bba0211ab741a009f45e4828c
Max North <maxnorth@hey.com>
2022-08-14 18:32:06 -0400

    publish

--- publish.ps1
+++ publish.ps1
@@ -1,12 +1,12 @@
 Remove-Item ./* -Exclude @("publish.ps1", "CNAME")
 
-Push-Location ../lcdk/dashboard
+Push-Location ../ctrlflow/dashboard
 
 npm run demo:build
 
 Pop-Location
 
-Copy-Item ../lcdk/dashboard/demo/dist/* .
+Copy-Item ../ctrlflow/dashboard/demo/dist/* .
 Copy-Item index.html 404.html
 
 git add -A
